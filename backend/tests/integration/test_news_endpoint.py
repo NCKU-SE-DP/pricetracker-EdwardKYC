2338a58a0e340b366ff72d14909d9dd81d8ada7c
EdwardKYC <nked1010154@gmail.com>
2024-11-13 22:07:00 +0800

    加上env 改ci.yaml

--- backend/tests/integration/test_news_endpoint.py
+++ backend/tests/integration/test_news_endpoint.py
@@ -4,10 +4,10 @@ from sqlalchemy import create_engine, StaticPool
 from sqlalchemy.orm import sessionmaker
 import json
 from jose import jwt
-from main import app
-from main import Base, NewsArticle, User, session_opener, user_news_association_table
-from main import NewsSumaryRequestSchema, PromptRequest
-from main import pwd_context
+from src.main import app
+from src.main import Base, NewsArticle, User, session_opener, user_news_association_table
+from src.main import NewsSumaryRequestSchema, PromptRequest
+from src.main import pwd_context
 from unittest.mock import Mock
 
 
