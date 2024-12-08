d812ac34712d27b48620a03b6d44662b2d59068a
EdwardKYC <nked1010154@gmail.com>
2024-12-08 21:06:24 +0800

    修改client刪除service

--- backend/src/ai_service/base.py
+++ backend/src/ai_service/base.py
@@ -1,5 +1,22 @@
 import abc
 from typing import List, Dict
+from pydantic import BaseModel, Field
+
+class MessagePassingInterface(BaseModel):
+    """
+    Represents the structure of a message sent to the LLM API.
+    """
+    system_content: str = Field(...)
+    user_content: str = Field(...)
+
+    @property
+    def to_dict(self):
+        value = [
+            {"role": "system", "content": f"{self.system_content}"},
+            {"role": "user", "content": f"{self.user_content}"},
+        ]
+        return value
+    
 
 
 class LLMClientBase(metaclass=abc.ABCMeta):
